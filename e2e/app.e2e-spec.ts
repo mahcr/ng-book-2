8c3fae6b2355a8f9df1a8f7580de59af2526a6c2
mariano alvarez <malvarezh92@gmail.com>
2016-10-05 21:51:58 -0600

    update to 2.0 and add sass support

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { NgBook2Page } from './app.po';
+import { Ng2Page } from './app.po';
 
-describe('ng-book-2 App', function() {
-  let page: NgBook2Page;
+describe('ng2 App', function() {
+  let page: Ng2Page;
 
   beforeEach(() => {
-    page = new NgBook2Page();
+    page = new Ng2Page();
   });
 
   it('should display message saying app works', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
   title = 'app works!';
